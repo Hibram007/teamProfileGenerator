1bddad519976cc0e8a1be063a30dfda154bb04d9
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2021-12-01 21:07:18 -0700

    start of test code written for employee file

---
+++ _tests_/employee.test.js
@@ -0,0 +1,11 @@
+const Employee = require("../lib/Employee");
+
+const testData = {
+    name:"testEmployee",
+    id: "0",
+    email: "testEmployee@gmail.com",
+};
+
+var testEmployee;
+
+beforeEach
