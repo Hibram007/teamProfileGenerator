4b7e6d1cca05a6ec03844a3c71c0a2fb66c995f7
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2021-12-02 19:53:03 -0700

    Employee test written

--- _tests_/employee.test.js
+++ _tests_/employee.test.js
@@ -1,11 +1,39 @@
+const { expect, beforeEach, test } = require("@jest/globals");
 const Employee = require("../lib/Employee");
 
 const testData = {
-    name:"testEmployee",
-    id: "0",
-    email: "testEmployee@gmail.com",
+  name: "Employee",
+  id: "0",
+  email: "testEmployee@gmail.com",
 };
+let employee;
 
-var testEmployee;
+beforeEach(() => {
+  employee = new Employee(
+    testData.name,
+    testData.id,
+    testData.email
+  );
+});
 
-beforeEach
+test("creates Employee object", () => {
+  expect(employee.name).toBe(testData.name);
+  expect(employee.id).toBe(testData.id);
+  expect(employee.email).toBe(testData.email);
+});
+
+test("returns name", () => {
+  expect(employee.getName()).toBe(testData.name);
+});
+
+test("returns id", () => {
+  expect(employee.getId()).toBe(testData.id);
+});
+
+test("returns email", () => {
+  expect(employee.getEmail()).toBe(testData.email);
+});
+
+test('returns role', ()=>{
+    expect(employee.getRole()).toBe('Employee');
+})
